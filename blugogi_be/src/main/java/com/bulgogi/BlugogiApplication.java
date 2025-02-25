ed647a5035f73c2bec02e5d493d7f133495e75b0
kumugu <rmarn018@gmail.com>
2025-02-25 22:08:58 +0900

    create be_project

---
+++ blugogi_be/src/main/java/com/bulgogi/BlugogiApplication.java
@@ -0,0 +1,13 @@
+package com.bulgogi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlugogiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlugogiApplication.class, args);
+	}
+
+}
---
+++ blugogi_be/src/test/java/com/bulgogi/BlugogiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bulgogi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlugogiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
